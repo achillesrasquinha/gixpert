9f43df6de812d44d7f98f1b6e69a86f64c2d802d
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-12-01 17:13:45 -0600

    fix: revert parallel

--- src/gixpert/data/__init__.py
+++ src/gixpert/data/__init__.py
@@ -7,11 +7,10 @@ import tqdm as tq
 from bpyutils.util.environ import getenv
 from bpyutils.util.system  import makedirs
 from bpyutils.util.array   import sequencify
-from bpyutils.util.types   import lmap, build_fn
-from bpyutils.util.ml      import get_data_dir
+from bpyutils.util.types   import lmap
+from bpyutils.util.string  import get_random_str
 from bpyutils._compat      import iteritems
 from bpyutils.log import get_logger
-from bpyutils import parallel
 
 import deeply.datasets as dd
 from   deeply.datasets.util import SPLIT_TYPES, split as split_datasets
@@ -33,58 +32,14 @@ DATASETS = (
     "hyper_kvasir_segmented"
 )
 
-IMAGE_WIDTH   = settings.get("image_width")
-IMAGE_HEIGHT  = settings.get("image_height")
+def get_data_dir(data_dir = None):
+    data_dir = data_dir \
+        or getenv("DATA_DIR", prefix = _PREFIX) \
+        or osp.join(PATH["CACHE"], "data")
 
-AUGMENTER     = iaa.Sequential([
-    dia.Combination([
-        iaa.Fliplr(1.0),
-        iaa.Flipud(0.5),
-        iaa.TranslateX(percent = (-0.15, 0.15)),
-        iaa.TranslateY(percent = (-0.15, 0.15)),
-        iaa.ScaleX((0.9, 1.2)),
-        iaa.ScaleY((0.9, 1.2)),
-        iaa.Rotate(rotate = (-30, 30)),
-        iaa.ShearX((-30, 30)),
-        iaa.ShearY((-30, 30)),
-        iaa.ElasticTransformation(alpha = (0, 30), sigma = 5.0)
-    ]),
-    iaa.Resize({ "width": IMAGE_WIDTH, "height": IMAGE_HEIGHT })
-])
+    makedirs(data_dir, exist_ok = True)
 
-def _augment_split(split_type, dataset_name, *args, **kwargs):
-    data_dir = get_data_dir(NAME, data_dir = kwargs.get("data_dir"))
-    check    = kwargs.get("check", False)
-
-    dataset  = dd.load(dataset_name, data_dir = data_dir, shuffle_files = False)
-
-    dataset  = split_datasets(dataset["data"], splits = (.8, .1, .1))
-    groups   = dict(zip(SPLIT_TYPES, dataset))
-
-    split_type, split = groups[split_type]
-
-    logger.info("Augmenting dataset %s of type %s..." % (dataset_name, split_type))
-    
-    dir_path   = osp.join(data_dir, split_type)
-
-    images_dir = osp.join(dir_path, "images")
-    masks_dir  = osp.join(dir_path, "masks")
-
-    if check:
-        split = split.take(3)
-
-    for data in tq.tqdm(split.batch(1)):
-        image, mask = data["image"].numpy(), data["mask"].numpy()
-        
-        augment_images(AUGMENTER, images = image, masks = mask,
-            dir_images = images_dir, dir_masks = masks_dir)
-
-def _augment_dataset(dataset_name, *args, **kwargs):
-    jobs = kwargs.get("jobs", settings.get("jobs"))
-
-    with parallel.pool(processes = jobs) as pool:
-        function_ = build_fn(_augment_split, dataset_name = dataset_name, *args, **kwargs)
-        list(pool.map(function_, SPLIT_TYPES))
+    return data_dir
 
 def get_datasets(check = False):
     dataset_names = DATASETS
@@ -95,7 +50,7 @@ def get_datasets(check = False):
     return dataset_names
 
 def get_data(data_dir = None, check = False, *args, **kwargs):
-    data_dir = get_data_dir(NAME, data_dir = data_dir)
+    data_dir = get_data_dir(data_dir)
     dataset_names = get_datasets(check = check)
 
     try:
@@ -106,8 +61,7 @@ def get_data(data_dir = None, check = False, *args, **kwargs):
         datasets = dd.load(*dataset_names, data_dir = data_dir)
 
 def preprocess_data(data_dir = None, check = False, *args, **kwargs):
-    jobs     = kwargs.get("jobs", settings.get("jobs"))
-    data_dir = get_data_dir(NAME, data_dir = data_dir)
+    data_dir = get_data_dir(data_dir)
 
     try:
         dops.download('dataset:latest', target_dir = data_dir)
@@ -116,11 +70,49 @@ def preprocess_data(data_dir = None, check = False, *args, **kwargs):
 
         dataset_names = get_datasets(check = check)
 
-        with parallel.no_daemon_pool(processes = jobs) as pool:
-            function_ = build_fn(_augment_dataset, data_dir = data_dir,
-                check = check, *args, **kwargs)
-            list(pool.imap(function_, dataset_names))
+        datasets = lmap(
+            lambda x: x["data"],
+            sequencify(dd.load(*dataset_names, data_dir = data_dir, shuffle_files = True))
+        )
 
+        width, height = settings.get("image_width"), settings.get("image_height")
+
+        augmenter  = iaa.Sequential([
+            dia.Combination([
+                iaa.Fliplr(1.0),
+                iaa.Flipud(0.5),
+                iaa.TranslateX(percent = (-0.15, 0.15)),
+                iaa.TranslateY(percent = (-0.15, 0.15)),
+                iaa.ScaleX((0.9, 1.2)),
+                iaa.ScaleY((0.9, 1.2)),
+                iaa.Rotate(rotate = (-30, 30)),
+                iaa.ShearX((-30, 30)),
+                iaa.ShearY((-30, 30)),
+                iaa.ElasticTransformation(alpha = (0, 30), sigma = 5.0)
+            ]),
+            iaa.Resize({ "width": width, "height": height })
+        ])
+
+        for i, dataset in enumerate(datasets):
+            dataset = split_datasets(dataset, splits = (.8, .1, .1))
+            groups  = dict(zip(SPLIT_TYPES, dataset))
+
+            for split_type, split in iteritems(groups):
+                dir_path   = osp.join(data_dir, split_type)
+
+                images_dir = osp.join(dir_path, "images")
+                masks_dir  = osp.join(dir_path, "masks")
+
+                if check:
+                    split = split.take(3)
+
+                logger.info("Augmenting dataset %s for type %s..." % (dataset_names[i], split_type))
+
+                for data in tq.tqdm(split.batch(1)):
+                    image, mask = data["image"].numpy(), data["mask"].numpy()
+                    
+                    augment_images(augmenter, images = image, masks = mask,
+                        dir_images = images_dir, dir_masks = masks_dir)
         config = [
             { "source": osp.join(data_dir, split_type), "destination": split_type }
                 for split_type in SPLIT_TYPES
